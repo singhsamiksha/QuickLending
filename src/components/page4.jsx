3a8fac5da344adb410f3c292443a030ead9265af
singhsamiksha <singhsamiksha877@gmail.com>
2025-03-06 17:09:02 +0530

    content is added

--- src/components/page4.jsx
+++ src/components/page4.jsx
@@ -1,6 +1,36 @@
+import * as React from 'react';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 function Page4(){
-    return(<>
-    </>)
+    return(<div style={{ display: "flex", justifyContent: "space-between"}}>
+        <div style={{}}>
+       
+       <p>Testimonial</p>
+        <h1>What People Think of Us</h1>
+        
+        <hr
+          style={{
+            backgroundColor: "blue",
+            border: "none",
+            borderRadius: "5px",
+            width: "50px",
+            padding: "2px 3px",
+          }}
+       
+        /> 
+        <p>Lorem ipsum dolor sit amet consectetur. Sodales risus suspendisse ullamcorper etiam vel mi eleifend nisi.</p>
+       </div>
+       <div style={{}}>
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/036/473/286/small_2x/ai-generated-happy-woman-on-transparent-background-png.png"/>
+       </div>
+       <div style={{}}>
+        <p>“James and his team are my go-to source for any transaction we are considering. They are courteous, efficient and quick to get you to the closing table.”</p>
+        <div style={{display: "flex", justifyContent: "space-between"}}>
+            <h4>Linda</h4>
+            <p>stars</p>
+        </div>
+       </div>
+    </div>)
 }
 
 export default Page4;
